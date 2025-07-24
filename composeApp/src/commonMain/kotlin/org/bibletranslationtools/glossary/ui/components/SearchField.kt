d268a94c5b066705439d130b423fd81f441e15cf
Maxim <murmax82@gmail.com>
2025-07-24 14:25:07 +0400

    Search field ui

---
+++ composeApp/src/commonMain/kotlin/org/bibletranslationtools/glossary/ui/components/SearchField.kt
@@ -0,0 +1,78 @@
+package org.bibletranslationtools.glossary.ui.components
+
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SearchField(
+    searchQuery: TextFieldValue,
+    onValueChange: (TextFieldValue) -> Unit,
+    onFocusChange: (Boolean) -> Unit
+) {
+    val focusManager = LocalFocusManager.current
+    var searchFocused by remember { mutableStateOf(false) }
+    val width by remember {
+        derivedStateOf { if (searchFocused) 200.dp else 60.dp }
+    }
+
+    CustomTextField(
+        value = searchQuery,
+        onValueChange = onValueChange,
+        singleLine = true,
+        trailingIcon = {
+            if (searchFocused) {
+                IconButton(
+                    onClick = {
+                        onValueChange(TextFieldValue(""))
+                        focusManager.clearFocus()
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Close,
+                        contentDescription = "Search"
+                    )
+                }
+            } else {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "Search"
+                )
+            }
+        },
+        textStyle = LocalTextStyle.current.copy(
+            fontSize = 18.sp
+        ),
+        shape = MaterialTheme.shapes.medium,
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = MaterialTheme.colorScheme.background,
+            unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+        ),
+        modifier = Modifier.width(width)
+            .onFocusChanged {
+                searchFocused = it.isFocused
+                onFocusChange(it.isFocused)
+            }
+    )
+}
--- composeApp/src/commonMain/kotlin/org/bibletranslationtools/glossary/ui/screen/BrowseScreen.kt
+++ composeApp/src/commonMain/kotlin/org/bibletranslationtools/glossary/ui/screen/BrowseScreen.kt
@@ -6,24 +6,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.relocation.BringIntoViewRequester
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Language
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -33,10 +29,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
@@ -48,7 +42,7 @@ import org.bibletranslationtools.glossary.data.Workbook
 import org.bibletranslationtools.glossary.ui.components.BookItem
 import org.bibletranslationtools.glossary.ui.components.BrowseTopBar
 import org.bibletranslationtools.glossary.ui.components.ChapterGrid
-import org.bibletranslationtools.glossary.ui.components.CustomTextField
+import org.bibletranslationtools.glossary.ui.components.SearchField
 import org.jetbrains.compose.resources.stringResource
 
 class BrowseScreen(
@@ -71,6 +65,7 @@ class BrowseScreen(
 
         var filteredBooks by remember { mutableStateOf(books) }
         var searchQuery by remember { mutableStateOf(TextFieldValue("")) }
+        var searchFocused by remember { mutableStateOf(false) }
 
         LaunchedEffect(Unit) {
             val initialBookIndex = books.indexOf(activeBook)
@@ -100,52 +95,35 @@ class BrowseScreen(
                 BrowseTopBar(
                     title = stringResource(Res.string.browse),
                     actions = {
-                        CustomTextField(
-                            value = searchQuery,
-                            onValueChange = {
-                                searchQuery = it
-                            },
-                            singleLine = true,
-                            trailingIcon = {
-                                Icon(
-                                    imageVector = Icons.Default.Search,
-                                    contentDescription = "Search"
-                                )
-                            },
-                            textStyle = LocalTextStyle.current.copy(
-                                fontSize = 18.sp
-                            ),
-                            shape = MaterialTheme.shapes.medium,
-                            colors = TextFieldDefaults.colors(
-                                focusedContainerColor = MaterialTheme.colorScheme.background,
-                                unfocusedContainerColor = MaterialTheme.colorScheme.surface,
-                                focusedIndicatorColor = Color.Transparent,
-                                unfocusedIndicatorColor = Color.Transparent,
-                            ),
-                            modifier = Modifier.widthIn(max = 160.dp)
+                        SearchField(
+                            searchQuery = searchQuery,
+                            onValueChange = { searchQuery = it },
+                            onFocusChange = { searchFocused = it }
                         )
 
                         Spacer(modifier = Modifier.width(5.dp))
 
-                        Button(
-                            onClick = { /* Handle language change */ },
-                            shape = MaterialTheme.shapes.medium,
-                            colors = ButtonDefaults.buttonColors(
-                                containerColor = MaterialTheme.colorScheme.background,
-                                contentColor = MaterialTheme.colorScheme.onBackground
-                            ),
-                            border = BorderStroke(
-                                1.dp,
-                                MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f)
-                            )
-                        ) {
-                            Icon(
-                                Icons.Default.Language,
-                                contentDescription = "Language",
-                                modifier = Modifier.size(20.dp)
-                            )
-                            Spacer(modifier = Modifier.width(8.dp))
-                            Text("ENG")
+                        if (!searchFocused) {
+                            Button(
+                                onClick = { /* Handle language change */ },
+                                shape = MaterialTheme.shapes.medium,
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = MaterialTheme.colorScheme.background,
+                                    contentColor = MaterialTheme.colorScheme.onBackground
+                                ),
+                                border = BorderStroke(
+                                    1.dp,
+                                    MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f)
+                                )
+                            ) {
+                                Icon(
+                                    Icons.Default.Language,
+                                    contentDescription = "Language",
+                                    modifier = Modifier.size(20.dp)
+                                )
+                                Spacer(modifier = Modifier.width(8.dp))
+                                Text("ENG")
+                            }
                         }
                     }
                 ) {
